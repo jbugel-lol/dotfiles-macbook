b0d53afb867e0bf7628f214d8ede77f74a737278
Jonas Bugelnig <115107409+jbugel-lol@users.noreply.github.com>
2023-08-10 13:17:07 +0200

    markdown-preview.nvim added

--- .config/nvim/plugins.lua
+++ .config/nvim/plugins.lua
@@ -5,8 +5,13 @@ local plugins = {
     config = function()
       require "custom.configs.presence"
     end,
+  },{
+      "iamcco/markdown-preview.nvim",
+      run = function() vim.fn['mkdp#util#install']() end,
+      ft = 'markdown',
+      build = 'cd app && npm install',
+      cmd = 'MarkdownPreview'
+    }
   },
-}
 
 return plugins
-
